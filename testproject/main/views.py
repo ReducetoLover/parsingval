a1be69e6bf76c45b09e4fa8b8d6a98719c3708c0
ReducertoLover <krasnovskii2001@mail.ru>
2021-10-25 21:59:59 +0700

    Initial commit

---
+++ testproject/main/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import task
+from .forms import TaskForm
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html', {'title': 'Главная страница'})
+
+def about(request):
+    return render(request, 'main/about.html', {'title': 'About'})
+
+def stock(request):
+    return render(request, 'main/stock.html', {'title': 'Акции'})
+
+def currency(request):
+    return render(request, 'main/currency.html', {'title': 'Валюты'})
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Form is incorrect!'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+
