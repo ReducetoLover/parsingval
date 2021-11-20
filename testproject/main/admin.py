53c7e4c1845e0fed4352ba309b08754ad33d3aa9
Nikita-Silent <55372945+Nikita-Silent@users.noreply.github.com>
2021-11-20 18:28:44 +0700

    Вот

--- testproject/main/admin.py
+++ testproject/main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import task
+from .models import task, Curse, Stocks
 
 
 # Register your models here.
 
 admin.site.register(task)
-
+admin.site.register(Curse)
+admin.site.register(Stocks)
 # test
