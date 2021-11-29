930c35fca1ec427740919206f078d674b711648f
ReducertoLover <krasnovskii2001@mail.ru>
2021-11-29 23:20:31 +0700

    csv+css

--- testproject/main/static/js/table.js
+++ testproject/main/static/js/table.js
@@ -16,12 +16,13 @@ function table()
 {
      SELECTED_Period = CUR_WEEK;
      Currency = () => SELECTED_Period[SELECTED_Currency];
-    var a = Currency().dates.length-1;
+    var a = R[SELECTED_Currency].dates.length-1;
 
 
 
 
-    while (a>=Currency().dates.length-7)
+
+    while (a>=R[SELECTED_Currency].dates.length-7)
     {
 
         var i =".date_"+ count;
@@ -30,7 +31,7 @@ function table()
 
             var date= document.querySelector(i);
             var cost= document.querySelector(k);
-            if (Currency().dates[a]==undefined && Currency().values[a]==undefined)
+            if (R[SELECTED_Currency].dates[a]==undefined && R[SELECTED_Currency].values[a]==undefined)
             {
 
                //date.remove();
@@ -39,8 +40,8 @@ function table()
             }
             else
             {
-                date.innerHTML=`${Currency().dates[a]}`;
-                cost.innerHTML=`${Currency().values[a]} руб`;
+                date.innerHTML=`${R[SELECTED_Currency].dates[a]}`;
+                cost.innerHTML=`${R[SELECTED_Currency].values[a]} руб`;
             }
             count--;
             a--;
