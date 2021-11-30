674eef47ca53108ecd43af0582dc368d15a8a137
Artem <pro100artm0001@mail.ru>
2021-11-30 20:29:23 +0700

    get year curse

--- testproject/main/static/js/scv_to_json.js
+++ testproject/main/static/js/scv_to_json.js
@@ -1,4 +1,66 @@
 var R = {};
+var R_week = {};
+var R_year = {};
+
+function getWeek() {
+    let c = R[SELECTED_Currency].dates.length >= 7 ? 7 : R[SELECTED_Currency].dates.length;
+    let len = R[SELECTED_Currency].dates.length-1;
+    R_week[SELECTED_Currency] = {};
+    R_week[SELECTED_Currency].dates = [];
+    R_week[SELECTED_Currency].values = [];
+
+    for (var i=0; i<c; i++){
+        R_week[SELECTED_Currency].dates[i] = R[SELECTED_Currency].dates[len-i];
+        R_week[SELECTED_Currency].values[i] = R[SELECTED_Currency].values[len-i];
+    }
+}
+
+function getYear() {
+
+    Object.keys(R).forEach((e) => {
+        R_year[e] = splitDaysByMonth(R[e]);
+    })
+}
+
+function splitDaysByMonth(arr) {
+    let m = "00";
+
+    let result = 0;
+    let sum = 0;
+    let count = arr.dates.length;
+
+    let toArr = {};
+    toArr.dates = [];
+    toArr.values = [];
+
+    arr.dates.map((date, i) => {
+        if(m !== date.split('-')[1]) {
+
+            if(sum>0){
+                result = sum/count;
+                console.log(`${result} = ${sum}/${count}`);
+                toArr.values.push(result.toFixed(2));
+                sum = 0;
+            }
+
+            m = date.split('-')[1];
+
+            count = 0;
+            toArr.dates.push(m);
+        }
+        sum += arr.values[i]*1;
+        count++;
+
+        if(i === arr.dates.length-1) {
+            result = sum/count;
+            console.log(`${result} = ${sum}/${count}`);
+            toArr.values.push(result.toFixed(2));
+        }
+    });
+
+    return toArr;
+}
+
 $(document).ready(function() {
     $.ajax({
         type: "GET",
@@ -27,6 +89,8 @@ function parse(d) {
         },
         complete: function(results, file) {
             FirstDraw();
+            getWeek();
+            getYear();
             console.log(R);
         }}
         )
