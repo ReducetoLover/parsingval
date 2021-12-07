557de88189ed334c45a7be8ca12a72067153a72e
Yurle <titan9102001@gmail.com>
2021-12-07 16:57:04 +0700

    F

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -74,18 +74,18 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
-  #  }
-#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'BD.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'BD.sqlite3',
+ #   }
+#}
 
 
 
