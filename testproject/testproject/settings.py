bf3a7d656e1d528b24aab9f6d91ac1867b503204
ReducertoLover <krasnovskii2001@mail.ru>
2021-12-07 16:12:56 +0700

    baze

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -74,12 +74,20 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+  #  }
+#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'BD.sqlite3',
     }
 }
+
+
 
 
 # Password validation
